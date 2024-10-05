c13356d1292e7fc083eeb01d1378d4dfd9784885
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2024-10-05 17:57:59 +0200

    [BME680] Add async reading methods and docs

--- src/sensors/BME680/BME680Sensor.h
+++ src/sensors/BME680/BME680Sensor.h
@@ -1,17 +1,17 @@
 /**
  * @class BME680Sensor
  * @brief A sensor class for the BME680 sensor, inheriting from the Sensor class template.
- * 
+ *
  * This class provides methods to initialize the sensor, read data synchronously and asynchronously,
  * and retrieve the sensor data.
- * 
+ *
  * @note The BME680 sensor is used for measuring temperature, humidity, pressure, and gas.
- * 
+ *
  * @file BME680Sensor.h
- * 
+ *
  * @see Sensor
  * @see BME680SensorData
- * 
+ *
  * @author alessandr.monticell4@studio.unibo.it
  * @author luca.pulga@studio.unibo.it
  */
@@ -20,7 +20,7 @@
 #include "const/pins.h"
 
 class BME680Sensor : public Sensor<BME680SensorData>
- {
+{
 public:
     BME680Sensor();
     ~BME680Sensor();
@@ -28,7 +28,25 @@ public:
     bool init() override;
     bool readData() override;
     BME680SensorData getData() override;
-    bool readData_async();
+
+    /**
+     * @brief Start an asynchronous reading of the sensor data.
+     *
+     * @return true if the reading started successfully
+     * @return false if the reading failed to start
+     */
+    bool readData_async_start();
+
+    /**
+     * @brief End an asynchronous reading of the sensor data.
+     *        If no reading is in progress, the method will
+     *        perform a blocking read.
+     * @return true if the reading ended successfully
+     * @return false if the reading failed to end
+     */
+    bool readData_async_end();
+
 private:
-    BME680SensorData data;
+    BME680SensorData data;  // sensor data object
+    Adafruit_BME680 bme680; // BME680 sensor object
 };
