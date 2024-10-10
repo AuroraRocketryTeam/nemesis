c873231d781de502abf2cc3ad2aa763a9558901f
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2024-10-10 16:41:11 +0200

    [SENSORS] Add log method to SensorData

--- src/sensors/SensorData.h
+++ src/sensors/SensorData.h
@@ -2,6 +2,7 @@
 #define SENSORDATA_H
 
 #include <Arduino.h>
+#include "utils/logger/CborLogger.h"
 /**
  * @brief An abstract base class for sensor data.
  */
@@ -10,7 +11,7 @@ class SensorData
 public:
     virtual ~SensorData() = default;
     virtual String toString() = 0;
-
+    virtual void log(CborLogger &logger) = 0;
 protected:
     // Costruttore protetto per impedire l'istanza diretta
     SensorData() {}
