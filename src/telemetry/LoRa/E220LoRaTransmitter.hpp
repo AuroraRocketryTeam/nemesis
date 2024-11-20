1605a7c5a38536078e854b03e1f44ad5e60e457f
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2024-11-20 16:53:23 +0100

    [TELEMETRY] Modify E220LoRaTransmitter class

--- src/telemetry/LoRa/E220LoRaTransmitter.hpp
+++ src/telemetry/LoRa/E220LoRaTransmitter.hpp
@@ -21,22 +21,22 @@ public:
      * @param m1Pin  M1 pin
      */
     E220LoRaTransmitter(ILogger *logger, HardwareSerial &serial, byte auxPin, byte m0Pin, byte m1Pin) : 
-                        logger(logger), serial(serial), auxPin(auxPin), m0Pin(m0Pin), m1Pin(m1Pin) {};
+                        logger(logger), serial(serial), auxPin(auxPin), m0Pin(m0Pin), m1Pin(m1Pin), transmitter(&serial, auxPin, m0Pin, m1Pin) {};
 
-    E220LoRaTransmitter(ILogger *logger, HardwareSerial &serial) {
-        E220LoRaTransmitter(logger, serial, -1, -1, -1);
-    };
+    E220LoRaTransmitter(ILogger *logger, HardwareSerial &serial) : 
+                        logger(logger), serial(serial), auxPin(-1), m0Pin(-1), m1Pin(-1), transmitter(&serial, -1, -1, -1) {};
 
     bool init() override;
-    bool init(unsigned long serialBaudRate);
+    bool init(unsigned long serialBaudRate, Configuration config);
     void transmit(std::variant<char *, String, std::string, nlohmann::json> data) override;
-    void configure(Configuration configuration);
-
+    bool configure(Configuration configuration);
+    ResponseStructContainer getConfiguration();
+    String getConfigurationString(Configuration configuration) const;
 private:
     ILogger *logger;
+    HardwareSerial serial;
     byte auxPin;
     byte m0Pin;
     byte m1Pin;
     LoRa_E220 transmitter;
-    HardwareSerial serial;
-}
+};
