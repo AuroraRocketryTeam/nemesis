611c8775c98e870ea17caf946dcb68868f40d289
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2025-02-14 16:50:28 +0100

    [SD] Change interface method signature

--- src/utils/logger/SD/SD-master.hpp
+++ src/utils/logger/SD/SD-master.hpp
@@ -13,6 +13,6 @@ public:
     bool openFile(std::string filename);
     bool closeFile();
     bool writeFile(std::string filename, std::variant<std::string, String, char *> content); // se true file trovato e scritto, se false file non trovato
-    char *readFile();                                                                        // stampa il contenuto del file. ritorna true se tutto ok senno no
+    char *readFile(std::string filename);                                                                        // stampa il contenuto del file. ritorna true se tutto ok senno no
     bool clearSD();                                                                          // cancella tutto il contenuto dell'sd
 };
