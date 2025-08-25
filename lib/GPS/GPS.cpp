57a4b64d0659d39205817039f17d90147bdaac54
IGieckI <giacomo.antonelli3@gmail.com>
2025-08-25 12:25:50 +0200

    [GPS] Rewrite of the GPS's getData function

--- lib/GPS/GPS.cpp
+++ lib/GPS/GPS.cpp
@@ -12,25 +12,33 @@ bool GPS::init() {
 }
 
 std::optional<SensorData> GPS::getData() {
-    if (!myGNSS.checkUblox()) {
-        return std::nullopt;
-    }
+    myGNSS.checkUblox();
 
-    if (myGNSS.getLatitude() != 0 && myGNSS.getLongitude() != 0) {
+    // Add debug information
+    int32_t lat = myGNSS.getLatitude();
+    int32_t lon = myGNSS.getLongitude();
+    uint8_t fixType = myGNSS.getFixType();
+    uint8_t satellites = myGNSS.getSIV();
+
+    // Check for valid fix (fix type 2 = 2D, 3 = 3D)
+    if (fixType >= 2 && lat != 0 && lon != 0) {
         SensorData data("GPS");
-        data.setData("latitude", myGNSS.getLatitude() / 10000000.0);   // degrees
-        data.setData("longitude", myGNSS.getLongitude() / 10000000.0); // degrees
+        data.setData("latitude", lat / 10000000.0);
+        data.setData("longitude", lon / 10000000.0);
         double altitude_ellipsoid = myGNSS.getAltitude() / 1000.0;
-        //double geoid_separation = myGNSS.getGeoidSeparation() / 100.0; // often in centimeters
-        //double altitude_msl = altitude_ellipsoid - geoid_separation;
         data.setData("altitude", altitude_ellipsoid);
-        data.setData("altitude", myGNSS.getAltitude() / 1000.0);       // meters
-        data.setData("speed", myGNSS.getGroundSpeed() / 1000.0 * 3.6); // km/h
-        data.setData("satellites", myGNSS.getSIV());
+        // Remove duplicate altitude line
+        data.setData("speed", myGNSS.getGroundSpeed() / 1000.0 * 3.6);
+        data.setData("satellites", satellites);
         data.setData("hdop", myGNSS.getHorizontalDOP() / 100.0);
+        data.setData("fixType", fixType);
 
         return data;
     }
+
+    // For debugging: you might want to log why it failed
+    // Serial.printf("GPS: No fix - Type: %d, Sats: %d, Lat: %d, Lon: %d\n", 
+    //               fixType, satellites, lat, lon);
 
     return std::nullopt;
 }
