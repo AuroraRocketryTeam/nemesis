115ac4374e88dd36fb0275831e63b495373e2280
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2025-10-02 16:17:21 +0200

    [GPS-TASK] Use logger and shared pointer for GPS task

--- lib/control/src/tasks/GpsTask.hpp
+++ lib/control/src/tasks/GpsTask.hpp
@@ -6,23 +6,28 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include <GPS.hpp>
+#include "Logger.hpp"
 
-class GpsTask : public BaseTask {
+class GpsTask : public BaseTask
+{
 public:
-    GpsTask(std::shared_ptr<SharedSensorData> sensorData, 
-        SemaphoreHandle_t sensorDataMutex) : 
-        BaseTask("GpsTask"),
-        sensorData(sensorData),
-        sensorDataMutex(sensorDataMutex) {        
-            gps.init();
-        }
+    GpsTask(std::shared_ptr<SharedSensorData> sensorData,
+            SemaphoreHandle_t sensorDataMutex, std::shared_ptr<ISensor> gps) : BaseTask("GpsTask"),
+                                                                               sensorData(sensorData),
+                                                                               dataMutex(sensorDataMutex),
+                                                                               gps(gps)
+    {
+        LOG_INFO("GpsTask", "Initialized with GPS: %s", gps ? "OK" : "NULL");
+        
+    }
     ~GpsTask() override;
 
 protected:
     void taskFunction() override;
-
+    void onTaskStart() override;
+    void onTaskStop() override;
 private:
     std::shared_ptr<SharedSensorData> sensorData;
-    SemaphoreHandle_t sensorDataMutex;
-    GPS gps;
+    SemaphoreHandle_t dataMutex;
+    std::shared_ptr<ISensor> gps;
 };
